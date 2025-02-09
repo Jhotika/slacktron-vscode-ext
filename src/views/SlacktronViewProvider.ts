411b3161371f42899a8467acfd4bc5290499dbe0
Fahim Hoque <fahimhoque@Fahims-MacBook-Pro.local>
2025-02-09 23:54:09 +0600

    focus mode

--- src/views/SlacktronViewProvider.ts
+++ src/views/SlacktronViewProvider.ts
@@ -1,8 +1,12 @@
 import * as vscode from "vscode";
 
-const FOCUS_TIME = 15; // Focus time in seconds
+const FOCUS_TIME = 15;
 
 export class SlacktronViewProvider implements vscode.WebviewViewProvider {
+  private webviewView?: vscode.WebviewView;
+  private timerEndTime?: number;
+  private timerInterval?: NodeJS.Timeout;
+
   constructor(private readonly _extensionUri: vscode.Uri) {}
 
   resolveWebviewView(
@@ -10,6 +14,8 @@ export class SlacktronViewProvider implements vscode.WebviewViewProvider {
     context: vscode.WebviewViewResolveContext,
     _token: vscode.CancellationToken
   ) {
+    this.webviewView = webviewView;
+
     webviewView.webview.options = {
       enableScripts: true,
       localResourceRoots: [this._extensionUri],
@@ -17,23 +23,70 @@ export class SlacktronViewProvider implements vscode.WebviewViewProvider {
 
     webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
 
+    webviewView.onDidChangeVisibility(() => {
+      if (webviewView.visible && this.timerEndTime) {
+        const timeLeft = this.timerEndTime - Date.now();
+        if (timeLeft > 0) {
+          webviewView.webview.postMessage({ 
+            type: 'updateTimer', 
+            timeLeft: Math.ceil(timeLeft / 1000) 
+          });
+        }
+      }
+    });
+
     webviewView.webview.onDidReceiveMessage((data) => {
       switch (data.type) {
+        case "startTimer":
+          this.startTimer();
+          break;
         case "timerComplete":
-          vscode.window.showInformationMessage("Time to meditate!", "Meditate Now")
-            .then(selection => {
-              if (selection === "Meditate Now") {
-                vscode.env.openExternal(vscode.Uri.parse("https://algodetox.com/meditate"));
-                webviewView.webview.postMessage({ type: 'hideNotifications' });
-              }
-            });
+          this.showMeditationPrompt();
           break;
         case "meditateClicked":
           vscode.env.openExternal(vscode.Uri.parse("https://algodetox.com/meditate"));
-          webviewView.webview.postMessage({ type: 'hideNotifications' });
+          this.hideNotifications();
           break;
       }
     });
+  }
+
+  private startTimer() {
+    if (this.timerInterval) {
+      clearInterval(this.timerInterval);
+    }
+    this.timerEndTime = Date.now() + (FOCUS_TIME * 1000);
+    this.timerInterval = setInterval(() => {
+      if (this.timerEndTime) {
+        const timeLeft = this.timerEndTime - Date.now();
+        if (timeLeft <= 0) {
+          clearInterval(this.timerInterval);
+          this.timerEndTime = undefined;
+          this.showMeditationPrompt();
+        } else if (this.webviewView?.visible) {
+          this.webviewView.webview.postMessage({ 
+            type: 'updateTimer', 
+            timeLeft: Math.ceil(timeLeft / 1000) 
+          });
+        }
+      }
+    }, 1000);
+  }
+
+  private showMeditationPrompt() {
+    vscode.window.showInformationMessage("Time to meditate!", "Meditate Now")
+      .then(selection => {
+        if (selection === "Meditate Now") {
+          vscode.env.openExternal(vscode.Uri.parse("https://algodetox.com/meditate"));
+          this.hideNotifications();
+        }
+      });
+  }
+
+  private hideNotifications() {
+    if (this.webviewView) {
+      this.webviewView.webview.postMessage({ type: 'hideNotifications' });
+    }
   }
 
   private _getHtmlForWebview(webview: vscode.Webview) {
@@ -70,41 +123,36 @@ export class SlacktronViewProvider implements vscode.WebviewViewProvider {
           </head>
           <body>
             <button class="button" id="focusMode">Focus mode</button>
-            <div id="timer"></div>
+            <div id="timer" class="hidden"></div>
             <button class="button hidden" id="meditateButton">Meditate</button>
   
             <script>
               const vscode = acquireVsCodeApi();
-              const FOCUS_TIME = ${FOCUS_TIME};
-              let timerInterval;
               
               window.addEventListener('message', event => {
-                if (event.data.type === 'hideNotifications') {
-                  document.getElementById('meditateButton').classList.add('hidden');
+                const message = event.data;
+                switch (message.type) {
+                  case 'updateTimer':
+                    const timerDiv = document.getElementById('timer');
+                    timerDiv.classList.remove('hidden');
+                    timerDiv.textContent = \`Time remaining: \${message.timeLeft} seconds\`;
+                    if (message.timeLeft <= 0) {
+                      timerDiv.classList.add('hidden');
+                      document.getElementById('meditateButton').classList.remove('hidden');
+                      vscode.postMessage({ type: 'timerComplete' });
+                    }
+                    break;
+                  case 'hideNotifications':
+                    document.getElementById('meditateButton').classList.add('hidden');
+                    break;
                 }
               });
               
               document.getElementById('focusMode').addEventListener('click', () => {
-                const timerDiv = document.getElementById('timer');
                 const meditateButton = document.getElementById('meditateButton');
-                let timeLeft = FOCUS_TIME;
-                
-                // Hide meditate button and show timer
                 meditateButton.classList.add('hidden');
-                timerDiv.classList.remove('hidden');
-                
-                clearInterval(timerInterval);
-                timerInterval = setInterval(() => {
-                  timerDiv.textContent = \`Time remaining: \${timeLeft} seconds\`;
-                  timeLeft--;
-                  
-                  if (timeLeft < 0) {
-                    clearInterval(timerInterval);
-                    timerDiv.classList.add('hidden');
-                    meditateButton.classList.remove('hidden');
-                    vscode.postMessage({ type: 'timerComplete' });
-                  }
-                }, 1000);
+                document.getElementById('timer').classList.remove('hidden');
+                vscode.postMessage({ type: 'startTimer' });
               });
 
               document.getElementById('meditateButton').addEventListener('click', () => {
